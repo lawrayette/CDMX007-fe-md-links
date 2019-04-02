a2376f0cc0bbfe895aaa39be01bffc4c06e7aef8
lawrayette <milhadasdepapel@gmail.com>
2019-04-02 08:54:50 -0600

    accede al readme y lo imprime en la consola

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const fs = require('fs');
-const path = require('path');
-const colors = require('colors');
+const fs = require("fs");
 const fetch = require('node-fetch');
-const file = require('./README.md');
+const colors = require('colors');
+const path = require('path');
+const archive = ('./README.md');
 
+
+const readingFile = () => {
+  fs.readFile(archive, 'utf-8', (error, data)=>{
+ if(error){
+   console.log('error')
+ } else {
+   console.log(data);
+ }
+  })
+}
+readingFile();
