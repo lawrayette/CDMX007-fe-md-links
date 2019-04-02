e5f0c76e1b2fc1e44fe1c7026fef45542ef2b7d6
lawrayette <milhadasdepapel@gmail.com>
2019-04-02 09:17:41 -0600

    impime ruta absoluta en la consola y el readme completo.

--- index.js
+++ index.js
@@ -4,14 +4,27 @@ const colors = require('colors');
 const path = require('path');
 const archive = ('./README.md');
 
+const resolveRoute = (archive) => {
+  if (path.isAbsolute(archive) === true) {
+     
+  } else {
+      console.log(path.resolve(archive).green);    
+     
+  };
+};
+resolveRoute(archive);
 
 const readingFile = () => {
-  fs.readFile(archive, 'utf-8', (error, data)=>{
- if(error){
-   console.log('error')
- } else {
-   console.log(data);
- }
+  fs.readFile(archive, 'utf-8', (error, data) => {
+    if (error) {
+      console.log('error')
+    } else {
+      console.log(data);
+    }
   })
 }
 readingFile();
+
+const links = () => {
+
+}
