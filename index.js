a71cfcf43bb591572172690f5dc8d507c13b232d
lawrayette <milhadasdepapel@gmail.com>
2019-04-02 11:00:15 -0600

    imprime los enlaces y el path absolute en la consola

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
+// Declaración de módulos de NODEjs
 const fs = require("fs");
 const fetch = require('node-fetch');
 const colors = require('colors');
 const path = require('path');
 const archive = ('./README.md');
 
+// Función que accede a la ruta absoluta del archivo
 const resolveRoute = (archive) => {
   if (path.isAbsolute(archive) === true) {
-     
+
   } else {
-      console.log(path.resolve(archive).green);    
-     
+    console.log(path.resolve(archive).bgGreen.black);
+
   };
 };
-resolveRoute(archive);
 
-const readingFile = () => {
+// Función para acceder  los enlaces del readme y los imprime en la consola. 
+const file = () => {
   fs.readFile(archive, 'utf-8', (error, data) => {
     if (error) {
       console.log('error')
     } else {
-      console.log(data);
+      const regExp = /(http:\/\/|https:\/\/|www\.)[^\s][^)]+/g;
+      const links = data.match(regExp);
+      console.log(links);
+
     }
   })
 }
-readingFile();
 
-const links = () => {
 
-}
+
+
+
+file();
+resolveRoute(archive);
